bf9ba81cc126a657f502fa1c6a3336d26cbe6cb0
Надюшка Рыбка <kitti-46@ya.ru>
2022-11-10 15:19:00 +0300

    added 58

---
+++ 58TaskHOME/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу,
+ которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+/*int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Введено не число. Повторите ввод.");
+    return result;
+}*/
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] CompositionMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    {
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    resultMatrix[i, j] = resultMatrix[i, j] + matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+        return resultMatrix;
+    }
+}
+int[,] matrix1 = InitMatrix(2, 2);
+PrintMatrix(matrix1);
+int[,] matrix2 = InitMatrix(2, 2);
+PrintMatrix(matrix2);
+int[,] resultMatrix = CompositionMatrix(matrix1, matrix2);
+PrintMatrix(resultMatrix);
